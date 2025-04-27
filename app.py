75b473d00d486480f34e462aa51ca7c205475bae
haitrth <haithanhtran00@gmail.com>
2025-04-27 09:36:11 +0700

    first build

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("🎬 ScriptBoost - Short Video Script Generator")
+content_topic = st.text_input("Enter a content topic (e.g., self-development, travel, investment):)")
+if st.button("Generate Scirpts")
+    st.write ("You entered: ", content_topic)
+    
